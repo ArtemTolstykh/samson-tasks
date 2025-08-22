0758e3672c998ae5702a40fd15bb681bcf87e9b6
[ArtemTolstykh] <[ar.tolstyx2015@yandex.ru]>
2025-08-22 23:16:21 +0300

    Выполнил задание 4.2 из курса интеграция дизайна и настройка платформы

---
+++ samson-tasks/test.php
@@ -0,0 +1,13 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("test");
+?><?$APPLICATION->IncludeComponent(
+	"bitrix:system.auth.form",
+	"auth",
+	Array(
+		"FORGOT_PASSWORD_URL" => "/user/",
+		"PROFILE_URL" => "/user/profile.php",
+		"REGISTER_URL" => "/user/registration.php",
+		"SHOW_ERRORS" => "N"
+	)
+);?><br><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
